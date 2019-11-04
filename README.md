2ec28d0a5c45803364c519e62cba5457da0bf28e
Nick Peddie <nfpeddie@gmail.com>
2019-11-03 16:30:43 -0800

    adding initial files, created by huynhicode repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lunas Creative
+
+We build great things.
+
+Co-founded by Leeland, Nick and Steph.
---
+++ config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteTitle: 'Lunas Creative', // <title>
+  manifestName: 'Lunas Creative',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/lunas-creative/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Lunas Creative',
+  subHeading: 'We build great things.',
+  // social
+  socialLinks: [
+    // {
+    //   icon: 'fa-github',
+    //   name: 'Github',
+    //   url: 'https://github.com/lunas-creative',
+    // },
+    // {
+    //   icon: 'fa-instagram',
+    //   name: 'Instagram',
+    //   url: 'https://instagram.com/lunas-creative',
+    // },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/lunas-creative',
+    // },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/lunas-creative',
+    // },
+    // {
+    //   icon: 'fa-linkedin',
+    //   name: 'LinkedIn',
+    //   url: 'https://linkedin.com/lunas-creative',
+    // },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:hello@lunascreative.com',
+    },
+  ],
+  // phone: '000-00000',
+  address: 'San Diego, CA',
+};
